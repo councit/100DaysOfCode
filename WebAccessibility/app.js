638b44b88a214a5269eeab4367887144436a952d
Taylor Council <councit@gmail.com>
2020-10-19 14:47:20 -0700

    added tab trapping

--- WebAccessibility/app.js
+++ WebAccessibility/app.js
@@ -5,8 +5,6 @@ const timerButtons = document.querySelector(".project-wrapper")
 const timerText = document.querySelector(".timer-text")
 
 
-
-
 keyboard.addEventListener('keypress', e => {
     console.log(e.target)
     if (e.target.innerText !==   "Space"){
@@ -17,8 +15,6 @@ keyboard.addEventListener('keypress', e => {
     }
 
 } )
-
-
 
 var myTimer;
 
@@ -39,10 +35,59 @@ timerButtons.addEventListener('keypress', e => {
     }
 })
 
-
-
-
 function startTimer() {
     let result = parseInt(timerText.innerText) + 1;
     timerText.innerText = result;   
 }
+
+const modalOpenBtn = document.querySelector('.open-modal-btn');
+const modal = document.querySelector('.modal-wrapper');
+const modalCloseBtn = document.querySelector('.close-modal-btn');
+const modalBtns = document.querySelector('.modal-btn-wrapper')
+let isModalOpen = false
+
+
+
+modalOpenBtn.addEventListener("click", open);
+modalCloseBtn.addEventListener("click", close);
+
+function open() {
+    let currentItem = document.activeElement;
+
+
+
+    modal.style.display = "block";
+
+    modal.addEventListener('keydown', trap);
+}
+
+function close() {
+    modal.style.display = "none";
+}
+
+
+function trap(e){
+    var focusEvents = modal.querySelectorAll('a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, [tabindex^="0"], [tabindex^="1"], [tabindex^="2"], [tabindex^="3"], [tabindex^="4"], [tabindex^="5"], [tabindex^="6"], [tabindex^="7"], [tabindex^="8"], [tabindex^="9"]')
+    focusEvents = Array.prototype.slice.call(focusEvents);
+    var firstItem = focusEvents[0];
+    var lastItem = focusEvents[focusEvents.length - 2];
+    if(e.keyCode === 9){
+        console.log("tab")
+        if(e.shiftKey) {
+            if(document.activeElement === firstItem){
+                e.preventDefault();
+                lastItem.focus();
+
+            }
+        }else {
+            if(document.activeElement === lastItem){
+                e.preventDefault();
+                firstItem.focus();
+            }
+        }
+    }
+
+}
+
+
+
